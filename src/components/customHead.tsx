a477c3784802d2e5f1065f9d775192b91118ee77
jeremy <jeremysilvasilva@gmail.com>
2021-09-02 23:19:48 -0400

    :sparkles: Added description and title metatag

--- src/components/customHead.tsx
+++ src/components/customHead.tsx
@@ -3,12 +3,18 @@ import favicon from '../public/favicon.ico';
 
 type Props = {
   title?: string;
+  description?: string;
 };
 
-const CustomHead = ({ title = 'Lecket' }: Props) => (
+const CustomHead = ({
+  title = 'Lecket',
+  description = 'Watch and download Movies and Series in Latin, Spanish, Subtitled and English, the latest releases in the best HD quality without cuts.',
+}: Props) => (
   <Head>
     <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
     <title>{title}</title>
+    <meta name="title" content={title} />
+    <meta name="description" content={`${description} Lecket Online`} />
   </Head>
 );
 
