5aff653ee33d1874d2f87540789cdb0551c02d90
jeremy <jeremysilvasilva@gmail.com>
2021-09-14 22:23:56 -0300

    :sparkles: Added useAuth hook and fixed login page

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -8,5 +8,5 @@ type Credentials = {
 export async function login(credentials: Credentials) {
   const { data, headers } = await instance.post('users/login', credentials);
   console.log(headers)
-  return { data };
+  return data;
 }
